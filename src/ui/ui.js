52d38880842048e4ce769342974a30843240033d
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-16 21:26:20 -0400

    Save textarea to local archive

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,16 +1,20 @@
 requirejs(["vendor/mithril"], function(mIgnore) {
     const root = document.body
 
-    const Hello = {
-        count: 0,
-        increment: function() { Hello.count++ },
+    const Archive = {
+        items: [],
+        add: function() { Archive.items.push(Archive.editorContents); Archive.editorContents = "" },
+        editorContents: "",
         view: function() {
             return m("main", [
-                m("h1", {class: "title bw24 b--solid b--blue"}, "Click count " + Hello.count),
-                m("button", { onclick: Hello.increment }, "Increment"),
+                m("h1", {class: "title bw24 b--solid b--blue"}, "Item count " + Archive.items.length),
+                m("textarea", { value: Archive.editorContents,  onchange: function (event) { Archive.editorContents = event.target.value } }),
+                m("br"),
+                m("button", { onclick: Archive.add }, "Add"),
+                m("button", { onclick: function () { console.log("items", Archive.items) }}, "Log"),
             ])
         }
     }
 
-    m.mount(root, Hello)
+    m.mount(root, Archive)
 })
