6c6cdb37be43dda684198455e7726dc1c9c98261
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-17 09:49:04 -0400

    Add support for clearing with confirm

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,7 +4,19 @@ requirejs(["vendor/mithril"], function(mIgnore) {
     const Archive = {
         items: [],
         add: function() { Archive.items.push(Archive.editorContents); Archive.editorContents = "" },
-        showLog: function () { console.log("items", Archive.items) },
+        confirmClear: function(promptText) {
+            if (!Archive.editorContents) return true
+            if (Archive.editorContents === Archive.textForLog()) return true;
+            if (!promptText) promptText = "You have unsaved editor changes; proceed?"
+            return confirm(promptText)
+        },
+        clear: function() { if (!Archive.confirmClear()) return; Archive.editorContents = "" },
+        textForLog: function() { return JSON.stringify(Archive.items, null, 4) },
+        showLog: function () {
+            console.log("items", Archive.items)
+            if (!Archive.confirmClear()) return
+            Archive.editorContents = Archive.textForLog(); 
+        },
         editorContents: "",
         view: function() {
             return m("main.ma2", [
@@ -13,6 +25,7 @@ requirejs(["vendor/mithril"], function(mIgnore) {
                 m("br"),
                 m("button.ma1", { onclick: Archive.add }, "Add"),
                 m("button.ma1", { onclick: Archive.showLog }, "Show log"),
+                m("button.ma1", { onclick: Archive.clear }, "Clear"),
             ])
         }
     }
