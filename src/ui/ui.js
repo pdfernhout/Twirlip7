29041acc6b67da6e56ae302079f6f312e14d193d
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-17 09:34:52 -0400

    Improve styling

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -6,12 +6,12 @@ requirejs(["vendor/mithril"], function(mIgnore) {
         add: function() { Archive.items.push(Archive.editorContents); Archive.editorContents = "" },
         editorContents: "",
         view: function() {
-            return m("main", [
-                m("h1", {class: "title bw24 b--solid b--blue"}, "Item count " + Archive.items.length),
+            return m("main.ma2", [
+                m("h1.title.bw24.b--solid.b--blue", "Item count " + Archive.items.length),
                 m("textarea", { value: Archive.editorContents,  onchange: function (event) { Archive.editorContents = event.target.value } }),
                 m("br"),
-                m("button", { onclick: Archive.add }, "Add"),
-                m("button", { onclick: function () { console.log("items", Archive.items) }}, "Log"),
+                m("button.ma1", { onclick: Archive.add }, "Add"),
+                m("button.ma1", { onclick: function () { console.log("items", Archive.items) }}, "Log"),
             ])
         }
     }
