b72275b935a635e514bbfb65b57542ffadcaa9d2
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-17 09:37:59 -0400

    Refactor showLog function

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,14 +4,15 @@ requirejs(["vendor/mithril"], function(mIgnore) {
     const Archive = {
         items: [],
         add: function() { Archive.items.push(Archive.editorContents); Archive.editorContents = "" },
+        showLog: function () { console.log("items", Archive.items) },
         editorContents: "",
         view: function() {
             return m("main.ma2", [
                 m("h1.title.bw24.b--solid.b--blue", "Item count " + Archive.items.length),
-                m("textarea", { value: Archive.editorContents,  onchange: function (event) { Archive.editorContents = event.target.value } }),
+                m("textarea", { value: Archive.editorContents, onchange: function (event) { Archive.editorContents = event.target.value } }),
                 m("br"),
                 m("button.ma1", { onclick: Archive.add }, "Add"),
-                m("button.ma1", { onclick: function () { console.log("items", Archive.items) }}, "Log"),
+                m("button.ma1", { onclick: Archive.showLog }, "Show log"),
             ])
         }
     }
