50dcd606f8ac6b4bb1f4ee5fd70d60b84dba3173
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-18 08:55:10 -0400

    Add file import

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -33,6 +33,39 @@ requirejs(["vendor/mithril"], function(mIgnore) {
             Archive.setEditorContents(Archive.editorContents + "\n" + eval(Archive.editorContents))
         },
 
+        importText: function() {
+            if (!Archive.confirmClear()) return
+            Archive.loadFromFile((fileName, fileContents) => {
+                if (fileContents) {
+                    console.log("updating editor")
+                    const newContent = fileName + "\n---------------------------------------\n" + fileContents;
+                    Archive.setEditorContents(newContent)
+                    m.redraw()
+                }
+            })
+        },
+
+        loadFromFile: function (callback) {
+            const fileControl = document.getElementById("fileInput");
+            fileControl.addEventListener("change", function (event) {
+                if (event.target.files.length < 1) return;
+                const file = event.target.files[0];
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    const contents = event.target.result;
+                    callback(file.name, contents);
+                };
+                
+                reader.onerror = function(event) {
+                    console.error("File could not be read! Code " + event.target.error.code);
+                    callback(null, null);
+                };
+                
+                reader.readAsText(file);
+            }, false);
+            fileControl.click();
+        },
+
         skip: function (offset) {
             if (!Archive.items.length) return
             Archive.previousNextIndex = (Archive.items.length + Archive.previousNextIndex + offset) % Archive.items.length
@@ -64,11 +97,13 @@ requirejs(["vendor/mithril"], function(mIgnore) {
         view: function() {
             return m("main.ma2", [
                 m("h1.title.bw24.b--solid.b--blue", "Item count " + Archive.items.length),
+                m("input#fileInput", { "type" : "file" , "hidden" : true } ),
                 m("textarea", { value: Archive.editorContents, onchange: function (event) { Archive.editorContents = event.target.value } }),
                 m("br"),
                 m("button.ma1", { onclick: Archive.add }, "Add"),
                 m("button.ma1", { onclick: Archive.clear }, "Clear"),
                 m("button.ma1", { onclick: Archive.eval }, "Eval"),
+                m("button.ma1", { onclick: Archive.importText }, "Import"),
                 m("br"),
                 m("button.ma1", { onclick: Archive.previous }, "Previous"),
                 m("button.ma1", { onclick: Archive.next }, "Next"),
