78712dac6163bedabe385109623f346e3064bd10
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-04-21 09:59:33 -0400

    Rename Workspace to WorkspaceView
    Change made inside component file to be more consistent

--- src/ui/WorkspaceView.js
+++ src/ui/WorkspaceView.js
@@ -5,32 +5,32 @@ define(["FileUtils", "SelectionUtils", "EvalUtils"], function(FileUtils, Selecti
         items: [],
     }
 
-    const Workspace = {
+    const WorkspaceView = {
         editorContents: "",
         lastLoadedContents: "",
         currentItemIndex: null,
 
         setEditorContents(newContents) {
-            Workspace.editorContents = newContents
-            Workspace.lastLoadedContents = newContents
+            WorkspaceView.editorContents = newContents
+            WorkspaceView.lastLoadedContents = newContents
         },
 
         save: function() {
-            Archive.items.push(Workspace.editorContents)
-            Workspace.currentItemIndex = Archive.items.length - 1
+            Archive.items.push(WorkspaceView.editorContents)
+            WorkspaceView.currentItemIndex = Archive.items.length - 1
         },
 
         confirmClear: function(promptText) {
-            if (!Workspace.editorContents) return true
-            if (Workspace.editorContents === Workspace.lastLoadedContents) return true
+            if (!WorkspaceView.editorContents) return true
+            if (WorkspaceView.editorContents === WorkspaceView.lastLoadedContents) return true
             if (!promptText) promptText = "You have unsaved editor changes; proceed?"
             return confirm(promptText)
         },
 
         clear: function() {
-            if (!Workspace.confirmClear()) return
-            Workspace.setEditorContents("")
-            Workspace.currentItemIndex = null
+            if (!WorkspaceView.confirmClear()) return
+            WorkspaceView.setEditorContents("")
+            WorkspaceView.currentItemIndex = null
         },
 
         doIt: function () {
@@ -44,9 +44,9 @@ define(["FileUtils", "SelectionUtils", "EvalUtils"], function(FileUtils, Selecti
 
         printIt: function () {
             const selection = SelectionUtils.getSelection("editor", true)
-            const contents = Workspace.editorContents
+            const contents = WorkspaceView.editorContents
             const evalResult = "" + EvalUtils.evalOrError(selection.text)
-            Workspace.editorContents = contents.substring(0, selection.end) + evalResult + contents.substring(selection.end)
+            WorkspaceView.editorContents = contents.substring(0, selection.end) + evalResult + contents.substring(selection.end)
             setTimeout(() => SelectionUtils.selectRange("editor", selection.end, selection.end + evalResult.length), 0)
         },
 
@@ -57,36 +57,36 @@ define(["FileUtils", "SelectionUtils", "EvalUtils"], function(FileUtils, Selecti
         },
 
         importText: function() {
-            if (!Workspace.confirmClear()) return
+            if (!WorkspaceView.confirmClear()) return
             FileUtils.loadFromFile((fileName, fileContents) => {
                 if (fileContents) {
                     console.log("updating editor")
                     const newContent = fileName + "\n---------------------------------------\n" + fileContents
-                    Workspace.setEditorContents(newContent)
+                    WorkspaceView.setEditorContents(newContent)
                     m.redraw()
                 }
             })
         },
 
         exportText: function() {
-            const fileContents = Workspace.editorContents
+            const fileContents = WorkspaceView.editorContents
             const provisionalFileName = fileContents.split("\n")[0]
             FileUtils.saveToFile(provisionalFileName, fileContents)
         },
 
         skip: function (offset) {
             if (!Archive.items.length) return
-            if (Workspace.currentItemIndex === null) {
-                offset >= 0 ? Workspace.currentItemIndex = 0 : Workspace.currentItemIndex = Archive.items.length
+            if (WorkspaceView.currentItemIndex === null) {
+                offset >= 0 ? WorkspaceView.currentItemIndex = 0 : WorkspaceView.currentItemIndex = Archive.items.length
             } else {
-                Workspace.currentItemIndex = (Archive.items.length + Workspace.currentItemIndex + offset) % Archive.items.length
+                WorkspaceView.currentItemIndex = (Archive.items.length + WorkspaceView.currentItemIndex + offset) % Archive.items.length
             }
-            Workspace.setEditorContents(Archive.items[Workspace.currentItemIndex])
+            WorkspaceView.setEditorContents(Archive.items[WorkspaceView.currentItemIndex])
         },
 
-        previous: function () { Workspace.skip(-1) },
+        previous: function () { WorkspaceView.skip(-1) },
 
-        next: function () { Workspace.skip(1) },
+        next: function () { WorkspaceView.skip(1) },
 
         textForLog: function() {
             return JSON.stringify(Archive.items, null, 4)
@@ -94,45 +94,45 @@ define(["FileUtils", "SelectionUtils", "EvalUtils"], function(FileUtils, Selecti
 
         showLog: function () {
             console.log("items", Archive.items)
-            if (!Workspace.confirmClear()) return
-            Workspace.setEditorContents(Workspace.textForLog()) 
+            if (!WorkspaceView.confirmClear()) return
+            WorkspaceView.setEditorContents(WorkspaceView.textForLog()) 
         },
 
         loadLog: function () {
             if (Archive.items.length && !confirm("Replace all items with entered text for a log?")) return
-            Archive.items = JSON.parse(Workspace.editorContents)
-            Workspace.currentItemIndex = null
+            Archive.items = JSON.parse(WorkspaceView.editorContents)
+            WorkspaceView.currentItemIndex = null
             // Update lastLoadedContents in case pasted in contents to avoid warning later since data was processed as intended
-            Workspace.lastLoadedContents = Workspace.editorContents
+            WorkspaceView.lastLoadedContents = WorkspaceView.editorContents
         },
 
         view: function() {
             return m("main.ma2", [
                 m("h4.bw24.b--solid.b--blue", 
                     "Current item " + 
-                    (Workspace.currentItemIndex === null ? "???" : Workspace.currentItemIndex + 1) +
+                    (WorkspaceView.currentItemIndex === null ? "???" : WorkspaceView.currentItemIndex + 1) +
                     " of " + Archive.items.length
                 ),
                 m("input#fileInput", { "type" : "file" , "hidden" : true } ),
-                m("textarea.w-90-ns.h5-ns#editor", { value: Workspace.editorContents, oninput: function (event) { Workspace.editorContents = event.target.value; Workspace.currentItemIndex = null } }),
+                m("textarea.w-90-ns.h5-ns#editor", { value: WorkspaceView.editorContents, oninput: function (event) { WorkspaceView.editorContents = event.target.value; WorkspaceView.currentItemIndex = null } }),
                 m("br"),
-                m("button.ma1", { onclick: Workspace.save }, "Save"),
-                m("button.ma1", { onclick: Workspace.clear }, "Clear"),
-                m("button.ma1", { onclick: Workspace.importText }, "Import"),
-                m("button.ma1", { onclick: Workspace.exportText }, "Export"),
+                m("button.ma1", { onclick: WorkspaceView.save }, "Save"),
+                m("button.ma1", { onclick: WorkspaceView.clear }, "Clear"),
+                m("button.ma1", { onclick: WorkspaceView.importText }, "Import"),
+                m("button.ma1", { onclick: WorkspaceView.exportText }, "Export"),
                 m("br"),
-                m("button.ma1", { onclick: Workspace.doIt }, "Do it"),
-                m("button.ma1", { onclick: Workspace.printIt }, "Print it"),
-                m("button.ma1", { onclick: Workspace.inspectIt }, "Inspect it"),
+                m("button.ma1", { onclick: WorkspaceView.doIt }, "Do it"),
+                m("button.ma1", { onclick: WorkspaceView.printIt }, "Print it"),
+                m("button.ma1", { onclick: WorkspaceView.inspectIt }, "Inspect it"),
                 m("br"),
-                m("button.ma1", { onclick: Workspace.previous }, "Previous"),
-                m("button.ma1", { onclick: Workspace.next }, "Next"),
+                m("button.ma1", { onclick: WorkspaceView.previous }, "Previous"),
+                m("button.ma1", { onclick: WorkspaceView.next }, "Next"),
                 m("br"),
-                m("button.ma1", { onclick: Workspace.showLog }, "Show log"),
-                m("button.ma1", { onclick: Workspace.loadLog }, "Load log")
+                m("button.ma1", { onclick: WorkspaceView.showLog }, "Show log"),
+                m("button.ma1", { onclick: WorkspaceView.loadLog }, "Load log")
             ])
         }
     }
 
-    return Workspace
+    return WorkspaceView
 })
