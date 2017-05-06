98511d583df7bea93a822607c4d19d2751a87204
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-05 20:21:56 -0700

    Create svg-example.js

---
+++ test/svg-example.js
@@ -0,0 +1,14 @@
+// svg example
+
+show(() => {
+    return m("svg", { width: 200, height: 200},
+        m("circle", {
+            cx: 100, 
+            cy: 100, 
+            r: 50, 
+            stroke: "green", 
+            "stroke-width": 4, 
+            fill: "orange",
+        })
+    )
+})
