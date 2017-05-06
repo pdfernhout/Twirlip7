d972c4a084c263041f15563c817448fb308a4aca
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-06 07:13:12 -0700

    Create svg-with-updating.js

---
+++ test/svg-with-updating.js
@@ -0,0 +1,28 @@
+// svg example with updating
+// Seems to flicker slightly
+
+let x = 100;
+let y = 100;
+
+function update() {
+    x += 1;
+    y -= 0.25;
+    m.redraw()
+}
+const interval = setInterval(update, 100);
+
+show(() => {
+    return m("svg", { width: 600, height: 200},
+        m("rect[height='200'][width='600'][x='0'][y='0']", {
+            style: {"stroke": "black", "fill": "none", "stroke-width": "1"}
+        }),
+        m("circle", {
+            cx: x, 
+            cy: y, 
+            r: 50, 
+            stroke: "green", 
+            "stroke-width": 4, 
+            fill: "orange",
+        })
+    )
+}, "", () => clearInterval(interval))
