4a9bed373b14fdbf825d73f4574e2ae88303111b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-12 11:21:59 -0400

    Create modal-dialog.js

---
+++ test/modal-dialog.js
@@ -0,0 +1,42 @@
+// modal example
+
+let modalVisible = false
+
+function showModal(content) {
+    return m("div", [
+        m("div", {
+            style: {
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                "background-color": "rgba(0,0,0,0.6)",
+                "z-index": 10000,
+            }
+        }),
+        m("div", {
+            style: {
+                background: "#fff",
+                border: "1px solid #eee",
+                position: "fixed",
+                top: "100px",
+                left: "100px",
+                width: "500px",
+                height: "500px",
+                "z-index": 10001,
+            }
+        }, [
+            content, 
+            m("br"), 
+            m("button", {onclick: () => modalVisible = false}, "Close modal")
+        ])
+    ])
+}
+
+show(() => {
+    return m("div", [
+        m("button", {onclick: () => modalVisible = true}, "Open modal"),
+        modalVisible ? showModal(m("div", ["My Modal", m("hr"), new Date().toISOString()])) : "Modal closed"
+    ])
+})
