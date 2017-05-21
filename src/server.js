087a154883bed141393f02a667d40717905b43f1
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-21 08:55:25 -0400

    Update server.js to remove eslint warnings
    Also display localhost URL

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-var express = require('express')
+/* eslint-env node */
+
+var express = require("express")
 var app = express()
 
 /*
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get("/", function (req, res) {
+  res.send("Hello World!")
 })
 */
 
-app.use(express.static(__dirname + '/ui'));
+app.use(express.static(__dirname + "/ui"))
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log("Twirlip7 app listening on port 3000")
+    console.log("http://localhost:3000/")
 })
