4754cc49051714fd7d73357b611b5228ffe0c5cf
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-27 17:25:16 +0000

    Use a configuration file to order exampleJournal entries in buildExampleJournal.js

--- utils/buildExampleJournal.js
+++ utils/buildExampleJournal.js
@@ -3,41 +3,11 @@
 /* global require, __dirname */
 
 // This generates exampleJournal.js as a list of examples in a specific order
-
-const outputFileName = "../src/ui/exampleJournal.js"
+// A configuration file defines the files to include and the order
 
 const inputDirectory = "../examples/"
-
-const inputFileNames = `
-alert-hello-world.js
-one-plus-one.js
-my-add.js
-counter-needs-reloading.js
-counter-closable.js
-counter-closable-styled.js
-counter-component-up-down.js
-show-hello.js
-temperature-converter.js
-simple-done-app.js
-currency-converter-three.js
-currency-converter-many.js
-iframe-example.js
-modal-dialog.js
-three-3D.js
-svg-example.js
-svg-with-updating.js
-svg-with-dragging.js
-svg-multi-drag.js
-draggables-extended.js
-draggables-better.js
-draggable-simplified.js
-extension-simple.js
-extension-search.js
-journal-adding.js
-markdown-example.md
-html-example.html
-fortran-example.txt
-`
+const exampleJournalConfigurationFileName = "_exampleJournalConfiguration.txt"
+const outputFileName = "../src/ui/exampleJournal.js"
 
 const fileTemplateStart = `define([], function() {
     return [`
@@ -49,13 +19,16 @@ const fileTemplateEnd = `
  
 const fs = require("fs")
 
+const inputFileNames = fs.readFileSync(__dirname + "/" + inputDirectory + exampleJournalConfigurationFileName).toString().split("\n")
+
 const output = []
 output.push(fileTemplateStart)
 
 let needsLeadingComma = false
 
-for (let fileName of inputFileNames.split("\n")) {
+for (let fileName of inputFileNames) {
     if (!fileName) continue
+    if (fileName.startsWith("//")) continue
     const fullFileName = fs.realpathSync(__dirname + "/" + inputDirectory + fileName)
     console.log("reading", fullFileName)
     const fileContents = fs.readFileSync(fullFileName).toString()
@@ -69,8 +42,6 @@ for (let fileName of inputFileNames.split("\n")) {
 output.push(fileTemplateEnd)
 
 const outputResult = output.join("")
-
-// console.log(outputResult)
 
 const realOutputFileName = fs.realpathSync(__dirname + "/" + outputFileName)
 
