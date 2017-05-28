5e33ebdeedeec8aed62e5c00192d6836827c16c7
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-28 16:49:49 +0000

    Improve journal adding example

--- examples/journal-adding.js
+++ examples/journal-adding.js
@@ -1,1 +1,5 @@
-Twirlip7.getCurrentJournal().addItem("// Test of making a journal item from eval\n\nalert(\"hello world!\")")
+// Test of making a journal item from eval
+// The number of journal items should go up by one the first time you run this.
+// The new journal item will be the (new) last journal item.
+
+Twirlip7.getCurrentJournal().addItem("// Journal item made from eval\n\nalert(\"hello world!\")")
