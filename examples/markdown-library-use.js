5cfe513ab38e78e65fa61783528996db4c7af2ed
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-29 18:06:21 +0000

    Shorten first markdown code example

--- examples/markdown-library-use.js
+++ examples/markdown-library-use.js
@@ -7,16 +7,6 @@ Examples of using __markdown__.
 
 \`console.log("hello");\`
 
-### THINK
-
-Before you speak:
-
-* T - is it *true*?
-* H - is it *helpful*?
-* I - is it *inspiring*?
-* N - is it *necessary*?
-* K - is it *kind*?
-
 ### Quote
 
 > "The biggest challenge of the 21st century is the irony of technologies of abundance in the hands of those still thinking in terms of scarcity."<br>(Paul D. Fernhout)
