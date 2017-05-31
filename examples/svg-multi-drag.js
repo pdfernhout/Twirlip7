826e5648dcb31e7fa2d2ba6b99650fa0d11c6cc0
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-05-31 14:30:56 +0000

    Improve svg multi drag example

--- examples/svg-multi-drag.js
+++ examples/svg-multi-drag.js
@@ -1,6 +1,4 @@
 // SVG example with dragging of three circles
-// May not keep up with fast drags
-// Also issues when drag lower-Z circle over upper-Z circle
 
 let draggedItem = null
 let dragStart = {}
@@ -13,13 +11,13 @@ function onmousedownCircle(event) {
 }
 
 function onmousemoveCircle(event) {
-    if (draggedItem === event.target) { 
+    if (draggedItem) { 
         const dx = event.clientX - dragStart.x
         const dy = event.clientY - dragStart.y
         const newX = objectStart.x + dx
         const newY = objectStart.y + dy
-        event.target.setAttribute("cx", newX)
-        event.target.setAttribute("cy", newY)
+        draggedItem.setAttribute("cx", newX)
+        draggedItem.setAttribute("cy", newY)
     }
 }
 
@@ -28,9 +26,14 @@ function onmouseupCircle() {
 }
 
 Twirlip7.show(() => {
-    return m("svg", { width: 600, height: 200},
+    return m("svg", {
+            width: 600,
+            height: 200,
+            onmousemove: onmousemoveCircle,
+            onmouseup: onmouseupCircle,
+        },
         m("rect[height='200'][width='600'][x='0'][y='0']", {
-            style: {"stroke": "black", "fill": "none", "stroke-width": "1"}
+            style: {"stroke": "black", "fill": "none", "stroke-width": "1"},
         }),
         m("circle", {
             cx: 100, 
@@ -40,8 +43,6 @@ Twirlip7.show(() => {
             "stroke-width": 4, 
             fill: "orange",
             onmousedown: onmousedownCircle,
-            onmousemove: onmousemoveCircle,
-            onmouseup: onmouseupCircle,
         }),
         m("circle", {
             cx: 200, 
@@ -51,8 +52,6 @@ Twirlip7.show(() => {
             "stroke-width": 2, 
             fill: "yellow",
             onmousedown: onmousedownCircle,
-            onmousemove: onmousemoveCircle,
-            onmouseup: onmouseupCircle,
         }),
         m("circle", {
             cx: 400, 
@@ -62,8 +61,6 @@ Twirlip7.show(() => {
             "stroke-width": 7, 
             fill: "green",
             onmousedown: onmousedownCircle,
-            onmousemove: onmousemoveCircle,
-            onmouseup: onmouseupCircle,
         })
     )
 })
