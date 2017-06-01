b72862c899eeb2da3b6ff6c897d51d1c5ee7ae4e
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-06-01 02:56:03 +0000

    More progress on IBIS application to add arrows

--- examples/ibis_application.js
+++ examples/ibis_application.js
@@ -5,15 +5,27 @@
 
 // This is an unfinished work in progress
 
+// references:
+// arrow marker: https://stackoverflow.com/questions/12680166/how-to-use-an-arrow-marker-on-an-svg-line-element
+// arrowhead derived from: https://stackoverflow.com/questions/11808860/how-to-place-arrow-head-triangles-on-svg-lines
+// marker-end: http://tutorials.jenkov.com/svg/marker-element.html
+// line to edge of circle: https://stackoverflow.com/questions/13165913/draw-an-arrow-between-two-circles#13234898
+
 /* global CompendiumIcons */
 
 const diagram = []
+
+// tiny stack for connectign items
+let firstDraggedItem = null
+let secondDraggedItem = null
 
 let draggedItem = null
 let dragStart = {}
 let objectStart = {}
 
 function onmousedown(element, event) {
+    firstDraggedItem = secondDraggedItem
+    secondDraggedItem = element
     draggedItem = element
     dragStart = { x: event.clientX, y: event.clientY }
     objectStart = { x: element.x, y: element.y }
@@ -43,24 +55,73 @@ function newY() {
 }
 
 function addElement(type) {
-    const name = prompt(type + " name")
+    const name = type // prompt(type + " name")
     if (!name) return
     diagram.unshift({ type: type, name: name, x: newX(), y: newY() } )
+}
+
+function addLink() {
+    if (!firstDraggedItem) return
+    if (!secondDraggedItem) return
+    firstDraggedItem.parent = secondDraggedItem
+}
+
+function viewLink(element) {
+    const parent = element.parent
+    if (!parent) return
+    
+    const xA = parent.x
+    const yA = parent.y
+    const xB = element.x
+    const yB = element.y
+    const radius = 24
+    
+    const d = Math.sqrt((xB - xA) * (xB - xA) + (yB-yA) * (yB-yA))
+    const d2 = d - radius
+    
+    const ratio = d2 / d
+    
+    const dx = (xB - xA) * ratio
+    const dy = (yB - yA) * ratio
+    
+    const x = xA + dx
+    const y = yA + dy
+
+    return m("line", {
+        x1: x,
+        y1: y,
+        x2: element.x - dx,
+        y2: element.y - dy,
+        "marker-end": "url(#arrowhead)",
+        stroke: "black", 
+        "stroke-width": 1
+    })
 }
 
 function viewElement(element) {
     return [
         m("image", {
             "xlink:href": CompendiumIcons[element.type + "_png"],
-            x: element.x,
-            y: element.y,
+            x: element.x - 16,
+            y: element.y - 16,
             width: 32,
             height: 32,
             alt: "question",
             onmousedown: (event) => onmousedown(element, event)
         }),
-        m("text", {x: element.x + 16, y: element.y + 50, "text-anchor": "middle"}, element.name)
+        m("text", {x: element.x, y: element.y + 34, "text-anchor": "middle"}, element.name)
     ]
+}
+
+function viewArrowhead() {
+    return m("marker", {
+        id: "arrowhead",
+        orient: "auto",
+        markerWidth: 8,
+        markerHeight: 16,
+        refX: 2,
+        refY: 4,
+    }, m("path", { d: "M0,0 V8 L8,4 Z", fill: "black" }))
 }
 
 Twirlip7.show(() => {
@@ -69,8 +130,13 @@ Twirlip7.show(() => {
         m("button.ma1", { onclick: addElement.bind(null, "position") }, "Add position"),
         m("button.ma1", { onclick: addElement.bind(null, "plus") }, "Add plus"),
         m("button.ma1", { onclick: addElement.bind(null, "minus") }, "Add minus"),
+        m("button.ma1", { onclick: addLink }, "Link last two"),
         m("br"),
         m("svg.diagram.ba", { width: 600, height: 300, onmousemove: onmousemove, onmouseup: onmouseup, },
+            viewArrowhead(),
+            diagram.map((element) => {
+                return viewLink(element)
+            }),
             diagram.map((element) => {
                 return viewElement(element)
             })
