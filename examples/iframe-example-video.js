fb1c36d7bdd3a53acef55bbab249c388137cfdf3
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-06-08 01:48:23 +0000

    Adding example with videos

---
+++ examples/iframe-example-video.js
@@ -0,0 +1,16 @@
+// Mithril component which embeds three iframes with HTML videos
+
+const MyComponent = {
+    view(controller, args) {
+        return m("div.ba.ma3.pa3.bg-light-purple", [
+            // Passengers 2016 - Starship Avalon Slingshot Around The Red Giant Star Arcturus
+            m("iframe", {src: "//www.youtube.com/embed/1jGRpuzrkpk", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
+            // Alan Kay at OOPSLA 1997 - The computer revolution hasn't happened yet
+            m("iframe", {src: "//www.youtube.com/embed/oKg1hTOQXoY", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
+            // The Mother of All Demos, presented by Douglas Engelbart (1968)
+            m("iframe", {src: "//www.youtube.com/embed/yJDv-zdhzMY", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
+        ])
+    }
+}
+
+Twirlip7.show(MyComponent)
