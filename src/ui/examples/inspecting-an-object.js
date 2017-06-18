bc5ed20ed2f6c26d3e2053a89a7fdf2dfcb88029
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-06-18 13:21:30 +0000

    Improve instructions in inspecting an object for opening console

--- src/ui/examples/inspecting-an-object.js
+++ src/ui/examples/inspecting-an-object.js
@@ -1,10 +1,11 @@
 // If you try printing the below you would see "[object Object]"" which is not that useful.
-
 // Instead, you can click "Inspect it" to see the result in the JavaScript console where you can look inside the object.
 // That result is not displayed in the editor window -- it is only displayed in the console.
 
 // To open the console in various browsers, see:
 // https://webmasters.stackexchange.com/questions/8525/how-do-i-open-the-javascript-console-in-different-browsers
+// In most browsers, you can right-click on part of the web page and pick "Inspect" from the popup menu there
+// and then when the developer tools open, pick the console tab.
 
 // You can also try selecting just parts of the object (like the array) and use "Inspect It" or "Print it".
 
