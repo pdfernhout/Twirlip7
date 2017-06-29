4728c61e886a41e272b39163aa980d03f69ec0d1
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-06-29 02:51:18 +0000

    Replace a video in example because previous one disappeared

--- src/ui/examples/iframe-example-video.js
+++ src/ui/examples/iframe-example-video.js
@@ -3,13 +3,16 @@
 const MyComponent = {
     view(controller, args) {
         return m("div.ba.ma3.pa3.bg-light-purple", [
-            // Passengers 2016 - Starship Avalon Slingshot Around The Red Giant Star Arcturus
-            m("iframe", {src: "//www.youtube.com/embed/1jGRpuzrkpk", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
-            // Alan Kay at OOPSLA 1997 - The computer revolution hasn't happened yet
-            m("iframe", {src: "//www.youtube.com/embed/oKg1hTOQXoY", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
             // The Mother of All Demos, presented by Douglas Engelbart (1968)
             m("iframe", {src: "//www.youtube.com/embed/yJDv-zdhzMY", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
-            // Twirlip Civic Sensemaking Project Overview 
+            
+            // Alan Kay at OOPSLA 1997 - The computer revolution hasn't happened yet
+            m("iframe", {src: "//www.youtube.com/embed/oKg1hTOQXoY", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
+            
+            // The Lively Kernel presented by Dan Ingalls aa a Google Tech Talks  (2008)
+            m("iframe", {src: "//www.youtube.com/embed/gGw09RZjQf8", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
+            
+            // Twirlip Civic Sensemaking Project Overview (2011)
             m("iframe", {src: "//www.youtube.com/embed/_mRy4sGK7xk", width: 800, height: 600, frameborder: "0", allowfullscreen: true}),
         ])
     }
