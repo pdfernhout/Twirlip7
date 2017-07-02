cb2acf4eff3618cc890ff43c8db2dcf0d4b8d97f
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2017-07-02 14:16:20 +0000

    Add example of using font-awesome

---
+++ src/ui/examples/font-awesome-example.js
@@ -0,0 +1,53 @@
+// Example of using Font Awesome Icons included with the Twirlip7 application
+// Click on the "Font Awesome" link in the help header to lookup names for icons.
+
+Twirlip7.show(() => {
+    return m("div", [
+        // Using an icon directly and with a Twirlip7 convenience function
+        m("i.fa.fa-fast-forward[aria-hidden=true]"),
+        m("br"),
+        Twirlip7.icon("fa-fast-forward"),
+        
+        m("br"),
+        
+        // Using an icon in a button -- including with an extra Tachyons css class for styling the left margin
+        m("button", "Last", m("i.fa.fa-fast-forward[aria-hidden=true].ml1")),
+        m("br"),
+        m("button", "Last", Twirlip7.icon("fa-fast-forward.ml1")),
+        
+        m("br"),
+        
+        // Display a bunch of icons
+        someIcons.split("\n").map((iconName) => {
+            iconName = iconName.trim()
+            // Extra styling can also be applied as a second paraamter to the icon function
+            if (iconName) return Twirlip7.icon(iconName, ".ml2")
+            return []
+        })
+    ])
+})
+
+const someIcons = `
+    fa-glass
+    fa-music
+    fa-search
+    fa-envelope-o
+    fa-heart
+    fa-star
+    fa-star-o
+    fa-user
+    fa-film
+    fa-th-large
+    fa-th
+    fa-th-list
+    fa-check
+    fa-times
+    fa-search-plus
+    fa-search-minus
+    fa-power-off
+    fa-signal
+    fa-cog
+    fa-trash-o
+    fa-home
+    fa-file-o
+`
