f7d8ad140fe15f67cf7844578e43895b0d1fb710
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2019-03-31 10:14:54 -0400

    improve chat app

--- src/ui/chat.js
+++ src/ui/chat.js
@@ -1,65 +1,83 @@
-"use strict"
 /* global m */
 /* eslint-disable no-console */
 
-let chatRoom = "test"
-let userID = "anonymous"
-let chatText = ""
-const messages = []
+define(["/socket.io/socket.io.js", "NotebookBackendUsingServer", "vendor/mithril"], function(io, NotebookBackendUsingServer, mDiscard) {
+    "use strict"
 
-function chatRoomChange(event) {
-    chatRoom = event.target.value
-    // TODO: Other stuff for stream change
-}
+    console.log("NotebookBackendUsingServer", NotebookBackendUsingServer)
 
-function userIDChange(event) {
-    userID = event.target.value
-    // TODO: Other stuff for userID change
-}
+    let chatRoom = "test"
+    let userID = "anonymous"
+    let chatText = ""
+    const messages = []
 
-function chatTextChange(event) {
-    chatText = event.target.value
-}
+    let backend = NotebookBackendUsingServer(JSON.stringify({chatRoom}), userID)
 
-function sendChatMessage() {
-    const timestamp = new Date().toISOString()
-    const command = "insert"
-    const streamId = {chatRoom}
-
-    sendMessage({ command, streamId, chatText, userID, timestamp })
-    chatText= ""
-}
-
-function sendMessage(message) {
-    // TODO fix
-    messages.push(message)
-}
-
-const TwirlipChat = {
-    view: function () {
-        return m("div", [
-            m("h4.tc", "Twirlip Chat"),
-            m("div.ma1",
-                m("span.dib.tr.w4", "Chat room:"),
-                m("input", {value: chatRoom, onchange: chatRoomChange})
-            ),
-            m("div.ma1",
-                m("span.dib.tr.w4", "User ID:"),
-                m("input", {value: userID, onchange: userIDChange})
-            ),
-            messages.map(function (message) {
-                var localeTimestamp = new Date(Date.parse(message.timestamp)).toLocaleString()
-                return m("div", [
-                    m("span", {title: localeTimestamp, style: "font-size: 50%;"}, message.userID + ":"),
-                    " ",
-                    message.chatText
-                ])
-            }),
-            m("br"),
-            m("textarea", {value: chatText, onchange: chatTextChange}),
-            m("button", {onclick: sendChatMessage}, "Send")
-        ])
+    function chatRoomChange(event) {
+        chatRoom = event.target.value
+        messages.splice(0)
+        backend.configure(JSON.stringify({chatRoom}))
     }
-}
 
-window.onload = () => m.mount(document.body, TwirlipChat)
+    function userIDChange(event) {
+        userID = event.target.value
+        backend.configure(undefined, userID)
+    }
+
+    function chatTextChange(event) {
+        chatText = event.target.value
+    }
+
+    function sendChatMessage() {
+        const timestamp = new Date().toISOString()
+        const command = "insert"
+
+        sendMessage({ chatText, userID, timestamp })
+        chatText= ""
+    }
+
+    function sendMessage(message) {
+        // messages.push(message)
+        backend.addItem(message)
+    }
+
+    const TwirlipChat = {
+        view: function () {
+            return m("div", [
+                m("h4.tc", "Twirlip Chat"),
+                m("div.ma1",
+                    m("span.dib.tr.w4", "Chat room:"),
+                    m("input", {value: chatRoom, onchange: chatRoomChange})
+                ),
+                m("div.ma1",
+                    m("span.dib.tr.w4", "User ID:"),
+                    m("input", {value: userID, onchange: userIDChange})
+                ),
+                messages.map(function (message) {
+                    var localeTimestamp = new Date(Date.parse(message.timestamp)).toLocaleString()
+                    return m("div", [
+                        m("span", {title: localeTimestamp, style: "font-size: 50%;"}, message.userID + ":"),
+                        " ",
+                        message.chatText
+                    ])
+                }),
+                m("br"),
+                m("textarea", {value: chatText, onchange: chatTextChange}),
+                m("button", {onclick: sendChatMessage}, "Send")
+            ])
+        }
+    }
+
+    const responder = {
+        onLoaded: () => console.log("onLoaded"),
+        addItem: (item, isAlreadyStored) => {
+            console.log("addItem", item)
+            messages.push(item)
+        }
+    }
+
+    backend.connect(responder)
+    backend.setup(io)
+
+    window.onload = () => m.mount(document.body, TwirlipChat)
+})
