3d9832519296ecba4dede51bc55b103b220ca0f6
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2019-09-02 09:16:58 -0400

    notebook: improve dropdown menu to keep onscreen

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -13,10 +13,33 @@ export function popup(popupName, popupState, popupContent) {
 }
 
 export function menu(menuName, menuState) {
-    return m("div.relative.dib", { onmouseleave: () => menuState.isOpen = false  },
-        m("button", { onclick: () => menuState.isOpen = !menuState.isOpen }, menuName, m("span.ml2", "▾")),
+    return m("div.relative.dib",
+        { 
+            onmouseleave: () => menuState.isOpen = false
+        },
+        m("button", { 
+            onclick: (event) => {
+                menuState.isOpen = !menuState.isOpen 
+                const buttonBounds = event.target.getBoundingClientRect()
+                // TODO: Fix klunky menuHeight and menuWidth calculation assuming font size and padding
+                const menuHeight = 20 * (menuState.items.length + 2)
+                const menuWidth = 400
+                if (buttonBounds.bottom + menuHeight > window.innerHeight) {
+                    menuState.shouldDisplayAbove = true
+                } else {
+                    menuState.shouldDisplayAbove = false
+                }
+                if (buttonBounds.left + menuWidth > window.innerWidth && (buttonBounds.right - 400) >= 0) {
+                    menuState.shouldDisplayToLeft = true
+                } else {
+                    menuState.shouldDisplayToLeft = false
+                }
+            }
+        }, menuName, m("span.ml2", "▾")),
         menuState.isOpen ? m("div.absolute.bg-white.shadow-2.pa1.z-1", {
             style: {
+                right: menuState.shouldDisplayToLeft ? 0: undefined,
+                bottom: menuState.shouldDisplayAbove ? "1.25rem" : undefined,
                 display: (menuState.isOpen ? "block" : "none"),
                 "min-width": menuState.minWidth || "100%"
             }
